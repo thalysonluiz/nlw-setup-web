e015a174284710104ccc67a3183e8b009b779fca
Thalyson Luiz <thalysonluiz@gmail.com>
2023-01-20 12:00:02 -0300

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import './styles/global.css'
+
 import { Habit } from "./components/Habit"
+import { Header } from './components/Header'
 
 
 function App() {
 
 
   return (
-    <>
-      <Habit completed={1} />
-      <Habit completed={5} />
-      <Habit completed={6} />
-    </>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+      </div>
+
+    </div>
   )
 }
 
